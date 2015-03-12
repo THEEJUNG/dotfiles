32cfaf580aac68fc5c777fcd37f9760bbdfe597e
Dongyeop Kang <dykang85@gmail.com>
2015-03-12 21:04:30 +0900

    bashrc, htoprc added

--- install.py
+++ install.py
@@ -7,8 +7,8 @@ j = lambda *args: os.path.join(current_dir, *args)
 
 
 
-os.system("rm ~/.tmux.conf")
-jobs = [(j("vim"), "~/.vim"), (j("vimrc"), "~/.vimrc"), (j("gitconfig"), "~/.gitconfig"), (j("gitignore"), "~/.gitignore"), (j("tmux.conf"), "~/.tmux.conf")]
+os.system("mv ~/.tmux.conf ~/.tmux.conf.old")
+jobs = [(j("vim"), "~/.vim"), (j("vimrc"), "~/.vimrc"), (j("gitconfig"), "~/.gitconfig"), (j("gitignore"), "~/.gitignore"), (j("tmux.conf"), "~/.tmux.conf"), (j("htoprc"), "~/.config/htop/htoprc") ]
 
 for path, target in jobs:
     target = os.path.expanduser(target)
@@ -18,7 +18,13 @@ for path, target in jobs:
         os.symlink(path, target)
         print "created", target
 
+
+#os.system("sed -i '1s/^/if [ -f ~/work/dotfiles/bashrc ]; then . ~/work/dotfiles/bashrc ; fi' ~/.bashrc")
+
+os.system("echo 'if [ -f ~/work/dotfiles/bashrc ]; then . ~/work/dotfiles/bashrc ; fi' | cat - ~/.bashrc > temp && mv temp ~/.bashrc")
 os.system("tmux source-file ~/.tmux.conf")
+#os.system("source ~/.bashrc")
+
 
 import urllib
 urllib.urlretrieve("https://bootstrap.pypa.io/get-pip.py", filename="get-pip.py")
