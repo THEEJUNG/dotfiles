16c9cb46985b89c097b45068a68da05d6891dde1
Dongyeop Kang <dykang85@gmail.com>
2015-09-15 00:21:46 -0400

    update

--- install.py
+++ install.py
@@ -7,6 +7,16 @@ j = lambda *args: os.path.join(current_dir, *args)
 
 
 
+#os.system("sed -i '1s/^/if [ -f ~/work/dotfiles/bashrc ]; then . ~/work/dotfiles/bashrc ; fi' ~/.bashrc")
+
+os.system("echo 'if [ -f ~/work/dotfiles/bashrc ]; then . ~/work/dotfiles/bashrc ; fi' | cat - ~/.bashrc > temp && mv temp ~/.bashrc")
+os.system("tmux source-file ~/.tmux.conf")
+#os.system("source ~/.bashrc")
+
+
+import urllib
+urllib.urlretrieve("https://bootstrap.pypa.io/get-pip.py", filename="get-pip.py")
+
 os.system("mv ~/.tmux.conf ~/.tmux.conf.old")
 jobs = [(j("vim"), "~/.vim"), (j("vimrc"), "~/.vimrc"), (j("gitconfig"), "~/.gitconfig"), (j("gitignore"), "~/.gitignore"), (j("tmux.conf"), "~/.tmux.conf"), (j("htoprc"), "~/.config/htop/htoprc") ]
 
@@ -19,12 +29,3 @@ for path, target in jobs:
         print "created", target
 
 
-#os.system("sed -i '1s/^/if [ -f ~/work/dotfiles/bashrc ]; then . ~/work/dotfiles/bashrc ; fi' ~/.bashrc")
-
-os.system("echo 'if [ -f ~/work/dotfiles/bashrc ]; then . ~/work/dotfiles/bashrc ; fi' | cat - ~/.bashrc > temp && mv temp ~/.bashrc")
-os.system("tmux source-file ~/.tmux.conf")
-#os.system("source ~/.bashrc")
-
-
-import urllib
-urllib.urlretrieve("https://bootstrap.pypa.io/get-pip.py", filename="get-pip.py")
