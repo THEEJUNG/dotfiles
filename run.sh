43d596af562467c1b2da5d3ea4f1cb7b40d8c7a4
Dongyeop Kang <dykang85@gmail.com>
2016-11-07 15:13:04 -0500

    add nltk download script

--- run.sh
+++ run.sh
@@ -45,7 +45,6 @@ sudo chmod +x /usr/local/bin/gpustat
 #https://github.com/tensorflow/tensorflow/blob/master/tensorflow/g3doc/get_started/os_setup.md
 # For CPU-only version
 #pip install https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.5.0-cp27-none-linux_x86_64.whl
-
 # For GPU-enabled version (only install this version if you have the CUDA sdk installed)
 #pip install https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow-0.5.0-cp27-none-linux_x86_64.whl
 
